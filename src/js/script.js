da05d0506fc08eb0eb6a6f1b5dedf9fd9d8de32e
frideldev <fridel@gmail.com>
2024-03-28 19:32:21 -0400

    cambios finales v1.0

--- src/js/script.js
+++ src/js/script.js
@@ -68,6 +68,30 @@ botonca.onclick=()=>{
   }
   
 }
+let botonmeraki=document.querySelector('.botonmeraki');
+let completomeraki=document.querySelector('.completo-meraki');
+botonmeraki.onclick=()=>{
+  completomeraki.classList.toggle('activo');
+  if(botonmeraki.innerHTML=='Leer Menos'){
+    botonmeraki.innerHTML="Leer Mas";
+  }
+  else{
+    botonmeraki.innerHTML="Leer Menos";
+  }
+  
+}
+let botoneduavi=document.querySelector('.botoneduavi');
+let completoeduavi=document.querySelector('.completo-eduavi');
+botoneduavi.onclick=()=>{
+  completoeduavi.classList.toggle('activo');
+  if(botoneduavi.innerHTML=='Leer Menos'){
+    botoneduavi.innerHTML="Leer Mas";
+  }
+  else{
+    botoneduavi.innerHTML="Leer Menos";
+  }
+  
+}
 let sections = document.querySelectorAll('section');
 let navLinks= document.querySelectorAll('header nav a');
 
@@ -137,3 +161,58 @@ var swiper = new Swiper(".mySwiper", {
   ScrollReveal().reveal('.home__img img, .services__container, .portafolio__container__box, .testimonial-wrapper, .contact form', { origin: 'bottom' });
   ScrollReveal().reveal('.home__content__nombre, .about__img img', { origin: 'left' });
   ScrollReveal().reveal('.home__content__titulo, .home__content__parrafo, .about__content', { origin: 'right' });
+
+  document.getElementById("MensajeForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evitar el envío del formulario por defecto
+    
+    // Obtener los valores de los campos
+    var phoneNumber = '59167386760';
+    var nombre = document.getElementById("descripcion").value;
+    var message = document.getElementById("nombre").value;
+    var clientEmail = document.getElementById("email").value;
+    
+    // Formatear el número de teléfono para la API de WhatsApp (removiendo caracteres especiales)
+    phoneNumber = phoneNumber.replace(/\D/g, '');
+    
+    // URL de la API de WhatsApp
+    var whatsappURL = "https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + encodeURIComponent(message);
+    
+    // Construir el cuerpo del correo electrónico
+    var emailBody = "Mi Nombre es: " + nombre + "\n\n" + "Mensaje: " + message + "\n\n" + "Correo electrónico del cliente: " + clientEmail;
+    
+    // Codificar el cuerpo del correo electrónico para incluir saltos de línea
+    emailBody = encodeURIComponent(emailBody).replace(/%0A/g, "%0D%0A");
+    
+    // URL del servicio de correo electrónico (enviado a tu correo)
+    var emailURL = "mailto:fridel@gmail.com?subject=Mensaje desde la Pagina Web=" + emailBody;
+    
+    // Determinar qué botón se ha presionado
+    var submitType = event.submitter.name;
+    
+    if (submitType === "whastappButton") {
+      // Abrir una nueva ventana para enviar el mensaje por WhatsApp
+      window.open(whatsappURL);
+    } else if (submitType === "correoButton") {
+      // Redirigir al usuario a su cliente de correo electrónico con el mensaje preparado
+      window.location.href = emailURL;
+    }
+  });
+  
+  function abrirModal() {
+    // Ruta al archivo PDF (reemplaza con tu propia ruta)
+    const pdfUrl = 'src/doc/Edu-CV.pdf';
+
+    // Muestra el modal
+    document.getElementById('pdfModal').style.display = 'block';
+
+    // Establece la URL del <iframe>
+    document.getElementById('pdfIframe').src = pdfUrl;
+}
+
+function cerrarModal() {
+    // Oculta el modal
+    document.getElementById('pdfModal').style.display = 'none';
+
+    // Detiene la reproducción del PDF
+    document.getElementById('pdfIframe').src = '';
+}
